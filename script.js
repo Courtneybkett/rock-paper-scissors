5ca6c35fe1829884adf746033840a52a55be2c3d
Courtney Burkett <Courtneybkett@gmail.com>
2024-04-25 11:06:44 -0700

    added event listeners and html mods

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // ROCK PAPER SCISSORS
 const options = ['rock', 'paper', 'scissors'];
+const rockButton = document.querySelector('.rock')
+const paperButton  = document.querySelector('.paper')
+const scissorsButton = document.querySelector('.scissors')
+const outcomeDiv = document.querySelector('.outcome')
+
 
 function getComputerChoice() {
     const choice = options[Math.floor(Math.random() * options.length)];
     return choice;
-}
+};
 function checkWinner(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return 'tie';
@@ -18,27 +23,68 @@ function checkWinner(playerSelection, computerSelection) {
 else { 
     return 'computer';
 }
-}
-    
+};
 
 function playRound(playerSelection, computerSelection) {
     const result = checkWinner(playerSelection, computerSelection);
+    const p = document.createElement('p');
     if (result == 'tie') {
-        return 'its a tie!'
+        p.innerText = 'its a tie!'
+        outcomeDiv.appendChild(p)
     }
+    
     else if(result == 'player') {
-        return `you win! ${playerSelection} beats ${computerSelection}`
-    }
+        p.innerText = `you win! ${playerSelection} beats ${computerSelection}`
+        outcomeDiv.appendChild(p)
+     }
+    
     else {
-        return `you lose! ${computerSelection} beats ${playerSelection}`
+        p.innerText = `you lose! ${computerSelection} beats ${playerSelection}`
+        outcomeDiv.appendChild(p)
     }
-}
+};
 
-function playGame() {
-    for (let i = 0; i < 5; i++){
-    const playerSelection = 'rock';
-    const computerSelection = getComputerChoice();
-     console.log(playRound(playerSelection, computerSelection));
-    };
-}
-playGame()
+  // this whole check for winner might need to be removed, it was for the 
+  //purpose of creating a score tracker for each player but did not finish
+    const checkForWinner = (playerSelection, computerSelection) => {
+        if (playerSelection === 5) {
+            const h2 = document.createElement('h2')
+            h2.classList.add('player-won')
+            h2.innerText = `you won ${playerSelection} against ${computerSelection}`
+            outcomeDiv.append('h2')
+        } 
+        else if (computerSelection === 5) {
+            const h2 = document.createElement('h2')
+            h2.classList.add('computer-won')
+            h2.innerText = `you lost ${playerSelection} against ${computerSelection}`
+            outcomeDiv.append('h2')
+        }
+    }
+rockButton.addEventListener('click', () => {
+    const computerSelection = getComputerChoice()
+    const playerSelection = 'rock'
+    playRound(playerSelection, computerSelection)
+    checkForWinner(playerSelection, computerSelection)
+    
+});
+paperButton.addEventListener('click', () => {
+    const computerSelection= getComputerChoice()
+    const playerSelection= 'paper'
+    playRound(playerSelection, computerSelection)
+    checkForWinner(playerSelection, computerSelection)
+});
+scissorsButton.addEventListener('click', () => {
+    const computerSelection = getComputerChoice()
+    const playerSelection = 'scissors'
+    playRound(playerSelection, computerSelection)
+    checkForWinner(playerSelection, computerSelection)
+});
+//may want to remove the checkFOR winner from each button event listener
+//function playGame() {
+  //for (let i = 0; i < 5; i++){
+   //const playerSelection = 'rock';
+   //const computerSelection = getComputerChoice();
+    //console.log(playRound(playerSelection, computerSelection));
+   //};
+//}
+//playGame()
